9ef5ff5484d3a9b0aa08217479f07482db0a7608
Jonaid Bin Sharif <jonaidsharif101480@gmail.com>
2020-07-17 20:38:28 -0700

    done

--- assignment.js
+++ assignment.js
@@ -15,7 +15,7 @@ function tinyFriend(names){
     var smallest = names[0];
     for(var i=0; i < names.length; i++){
         var currentName = names[i];
-        if(currentName.length > smallest){
+        if(currentName < smallest){
             smallest = currentName;
         }
     }
@@ -23,7 +23,6 @@ function tinyFriend(names){
 }
 var smallest = tinyFriend(["jonaid","mastakim","elmaeza","ayub","mohammmad"]);
 console.log("Lowest value is: ", smallest);
-
 
 //tinyFriends area end
 
@@ -39,7 +38,29 @@ function woodCalculator(chair,table,bed){
 var woodResult = woodCalculator(5,3,5);
 console.log(woodResult);
 
-
 //woodCalculator area end
 
-//brickCalculator
+//brickCalculator area start
+
+function brickCalculator(floor){
+    let totalHeight;
+    let totalBrick;
+    if(floor<=10){
+        totalHeight = floor * 15;
+        totalBrick = totalHeight * 1000;
+    }   
+    else if(floor<=20){
+        totalHeight =(10 * 15)+(10 * 12);
+        totalBrick = totalHeight * 1000;
+    }
+    else{
+        moreFloor = 30-20;
+        totalHeight = (10 * 15)+(10 * 12)+(30 * 10);
+        totalBrick = totalHeight * 1000;
+        
+    }
+    return totalBrick;
+}
+console.log(brickCalculator(30));
+
+//brickCalculator area end
